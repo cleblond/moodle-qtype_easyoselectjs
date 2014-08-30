0d9020533dc041b45615dfbb0c43676343e6bbb9
Carl LeBlond <carl.leblond@iup.edu>
2014-08-30 10:55:16 -0400

    fixed language strings

--- lang/en/qtype_easyoselectjs.php
+++ lang/en/qtype_easyoselectjs.php
@@ -33,10 +33,10 @@ $string['pluginname_link'] = 'question/type/easyoselect';
 $string['pluginnameadding'] = 'Adding an Select Atoms or Molecules question';
 $string['pluginnameediting'] = 'Editing a Select Atoms or Molecules question';
 $string['pluginnamesummary'] = 'Students must select predefined atoms, molecules or functional groups.  You can ask questions such as; <ul><li>Select all the tertiary carbons below.</li><li>Select all chiral carbons in the following tsructure?</li><li>Select the longest chain of carbon atoms?</li></ul>';
-$string['easyoselectjs_options'] = 'Path to Marvin Applet installation';
-$string['enablejava'] = 'Tried but failed to load Marvinsketch editor. You have not got a JAVA runtime environment working in your browser. You will need one to attempt this question.';
-$string['enablejavaandjavascript'] = 'Loading Marvinsketch editor.... If this message does not get replaced by the Marvin editor then you have not got javascript and a JAVA runtime environment working in your browser.';
-$string['configeasyoselectjsoptions'] = 'The path of your marvin installation relative to your web root.  (e.g. If your moodle is installed at /var/www/moodle and you install your marvin at /var/www/marvin then you should use the default /marvin)';
+$string['easyoselectjs_options'] = 'Path to MarvinJS installation';
+$string['enablejava'] = 'Tried but failed to load MarvinJS editor. You have not got a JAVA runtime environment working in your browser. You will need one to attempt this question.';
+$string['enablejavaandjavascript'] = 'Loading Marvinsketch editor.... If this message does not get replaced by the MarvinJS editor then you have not got javascript and a JAVA runtime environment working in your browser.';
+$string['configeasyoselectjsoptions'] = 'The path of your MarvinJS installation relative to your web root.  (e.g. If your moodle is installed at /var/www/html/moodle and you install your MarvinJS at /var/www/html/marvinjs then you should use the default /marvinjs)';
 $string['filloutoneanswer'] = '<b><ul>
 <li>Fill out the question text above asking the student to select something</li>
 <li>Draw a structure in the Marvin Applet below.</li>
@@ -47,7 +47,7 @@ $string['filloutoneanswer'] = '<b><ul>
 </ul></b>';
 $string['filloutanswers'] = '<b><ul>
 <li>Fill out the question text above asking the student to select something</li>
-<li>Draw a structure in the Marvin Applet below.</li>
+<li>Draw a structure in the MarvinJS editor below.</li>
 <li>Select the molecules, atoms or objects you want the student to select.</li>
 <li>Press the "Insert from editor" button to insert the code into the answer box.</li>
 <li>Give the answer a grade.</li>
@@ -62,6 +62,6 @@ $string['correctansweris'] = 'The correct answer is: {$a}.';
 $string['correctanswers'] = 'Instructions';
 $string['notenoughanswers'] = 'This type of question requires at least {$a} answers';
 $string['pleaseenterananswer'] = 'Please enter an answer.';
-$string['easyoselectjseditor'] = 'MarvinSketch Editor';
+$string['easyoselectjseditor'] = 'MarvinJS Editor';
 $string['author'] = 'Question type courtesy of<br />Carl LeBlond,<br />Indiana University of Pennsylvania';
 $string['insert'] = 'Insert from editor';
